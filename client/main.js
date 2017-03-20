2d44344d00f4070b309f1ee9b9ca334436339479
Kai Wang <kai.wang.nankai@gmail.com>
2017-03-20 22:01:20 +1100

    Add capability to add advisers in the front end

--- client/main.js
+++ client/main.js
@@ -1,17 +1,53 @@
-list = function () {
+list_adviser = function () {
   response = jQuery.ajax({
-    url: "http://0.0.0.0:5667/api/people",
-    method: "get",
+    url: "http://0.0.0.0:5667/api/advisers",
+    method: "GET",
     dataType: "json",
-    success: handle_list_success,
-    error: handle_list_failure
+    success: _handle_list_adviser,
+    error: _handle_failure
   });
 };
 
-handle_list_success = function (response) {
+_handle_list_adviser = function (advisers) {
+  debugger;
+  var html_output = '';
+  advisers.forEach(
+    function (adviser) {
+      html_output += '<p>' + adviser.username + '</p>'
+    }
+  );
+  jQuery('#adviser_container').html(html_output);
+};
+
+add_adviser = function () {
+  var username = jQuery('#username').val();
+  var password = jQuery('#password').val();
+
+  if (!_validate(username) || !_validate(password)) {
+    alert('Invalid input!');
+    return;
+  }
+
+  jQuery.ajax({
+    url: "http://0.0.0.0:5667/api/advisers",
+    method: "POST",
+    data: {
+      "username": username,
+      "password": password
+    },
+    success: _handle_success,
+    error: _handle_failure
+  });
+};
+
+_validate = function (input_value) {
+  return !!input_value.trim().length;
+};
+
+_handle_success = function (response) {
   console.log(response);
 };
 
-handle_list_failure = function (XMLHttpRequest, textStatus, errorThrown) {
+_handle_failure = function (XMLHttpRequest, textStatus, errorThrown) {
   alert(textStatus + ": " + errorThrown);
 };
