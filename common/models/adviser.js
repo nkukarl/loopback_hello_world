a3dbc074ec987d20c9aa00eb1df0a45c1d9eb4e2
Kai Wang <kai.wang.nankai@gmail.com>
2017-03-22 23:24:04 +1100

    Learn to extend api methods and operate files

--- common/models/adviser.js
+++ common/models/adviser.js
@@ -1,5 +1,73 @@
 'use strict';
 
-module.exports = function(Adviser) {
+module.exports = function (Adviser) {
+  Adviser.status = function (ad) {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    var OPEN_HOUR = 6;
+    var CLOSE_HOUR = 20;
+    console.log("Current hour is %d", currentHour);
+    var response;
+    if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+      response = "Open for business!";
+    } else {
+      response = "Closed!";
+    }
+    ad(null, response);
+  };
+  Adviser.remoteMethod(
+    'status', {
+      http: {
+        path: '/status',
+        verb: 'get'
+      },
+      returns: {
+        arg: 'status',
+        type: 'string'
+      }
+    }
+  );
+
+  Adviser.file_read = function (ad) {
+    var fs = require('fs');
+    var fileList = './client/README.md';
+    fs.readFile(fileList, function (err, data) {
+      if (err) throw err;
+      console.log(data.toString());
+    });
+    ad(null, 'Done!');
+  };
+  Adviser.remoteMethod(
+    'file_read', {
+      http: {
+        path: '/file_read',
+        verb: 'get'
+      },
+      returns: {
+        arg: 'file_read',
+        type: 'string'
+      }
+    }
+  );
+
+  Adviser.file_write = function (ad) {
+    var fs = require('fs');
+    fs.writeFile('kwang_test.txt', 'hello world!!!', function (err) {
+      if (err) throw err;
+    });
+    ad(null, 'Done!');
+  };
+  Adviser.remoteMethod(
+    'file_write', {
+      http: {
+        path: '/file_write',
+        verb: 'post'
+      },
+      returns: {
+        arg: 'file_write',
+        type: 'string'
+      }
+    }
+  );
 
 };
